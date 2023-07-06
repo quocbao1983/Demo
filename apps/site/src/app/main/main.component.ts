0194ed99e9bbeaa190c5bb847703abf5d30049fc
quocbao1983 <quocbao280783@gmail.com>
2023-07-07 00:27:40 +0700

    update

--- apps/site/src/app/main/main.component.ts
+++ apps/site/src/app/main/main.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { TranslateService } from '@ngx-translate/core';
 import { NotifierService } from 'angular-notifier';
+import { ExchangeService } from '../shared/trans.service';
+import { ConfigService } from '../shared/config.service';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,6 +15,8 @@ import { NotifierService } from 'angular-notifier';
 })
 export class MainComponent implements OnInit {
   Detail: any = {};
+  Config: any = {};
+  ListsExchange: any[] = []
   Lists: any[] = []
   FilterLists: any[] = []
   Sitemap: any = { loc: '', priority: '' }
@@ -21,11 +28,24 @@ export class MainComponent implements OnInit {
     ru:'Russia',
     fr:'France',
   }
+  ListImg:any={
+    en:'assets/flag/en.png',
+    zh:'assets/flag/zh.png',
+    ko:'assets/flag/ko.png',
+    ru:'assets/flag/ru.png',
+    fr:'assets/flag/fr.png',
+  }
   @ViewChild('drawer', { static: true }) drawer!: MatDrawer;
+  displayedColumns: string[] = ['Email', 'CreateAt'];
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(
     private dialog: MatDialog,
     private _Notification: NotifierService,
-    public translate: TranslateService
+    public translate: TranslateService,
+    public _ConfigService: ConfigService,
+    private _ExchangeService: ExchangeService,
   ) {
     translate.addLangs(['en','zh','ko','ru','fr']);
     translate.setDefaultLang('en');
@@ -36,6 +56,17 @@ export class MainComponent implements OnInit {
     this.translate.use(event)
   }
   ngOnInit(): void {
+    this._ConfigService.getAll().subscribe(data=>this.Config = data[0])
+    this._ExchangeService.getAll().subscribe(data=>
+      {
+        this.ListsExchange = data
+        this.ListsExchange.forEach(v => {
+          v.Email = v.Email.replace(/(?<=.).(?=[^@]*?@)/g, "*");
+        });
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      })
     // this._RedirectService.getAll().subscribe((data)=>{
     //   console.log(data);
     //   this.FilterLists = this.Lists = data
