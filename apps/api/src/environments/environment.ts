8f25b7368995eb5ced29133959bd451793baf529
quocbao1983 <quocbao280783@gmail.com>
2023-07-29 09:25:40 +0700

    update

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: false,
+  DB_TEST:
+  {
+    type: 'mysql',
+    host: '103.221.220.31',
+    port: 3306,
+    username: 'bborycxy_demo',
+    password: '7UCkj65NgnW@tKK',
+    database: 'bborycxy_demo',
+    autoLoadEntities: true,
+    synchronize: true,
+    charset: "utf8mb4",
+  }
 };
